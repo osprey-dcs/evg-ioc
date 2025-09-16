3353ae6be419be9fa90f3cb402d54e8a7451fa25
Eric Norum <enorum@ospreydcs.com>
2025-09-15 19:08:18 -0700

    Add some records.

--- iocBoot/iocdev/st.cmd
+++ iocBoot/iocdev/st.cmd
@@ -1,15 +1,12 @@
 #!../../bin/linux-x86_64/evg
 
-#- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
-#-
-#- SPDX-License-Identifier: EPICS
-
-epicsEnvSet("P", "TST:")
+epicsEnvSet(EVG_IPV4, "$(EVG_IPV4=127.0.0.1)")
+epicsEnvSet("P", "EVG:")
 
 ## Register all support components
 dbLoadDatabase "../../dbd/evg.dbd"
 evg_registerRecordDeviceDriver(pdbbase) 
 
-dbLoadRecords("../../db/ospreyEVG.db","P=$(P),NAME=EVG,IPADDR=127.0.0.1")
+dbLoadRecords("../../db/ospreyEVG.db","P=$(P),NAME=EVG,IPADDR=$(EVG_IPV4)")
 
 iocInit()
