335298535f7cbe1af2e66dd8a8cb7e4c1d6464bf
Michael Davidsaver <mdavidsaver@gmail.com>
2026-02-01 19:42:50 -0800

    update OPIs

--- opi/runIt.sh
+++ opi/runIt.sh
@@ -3,8 +3,7 @@
 export JAVA_HOME="/Applications/CSS_Phoebus.app/jdk"
 PATH="$JAVA_HOME/Contents/Home/bin:$PATH"
 
-export P="EVG:"
+export P="EVT:"
 
 export arch="$EPICS_HOST_ARCH"
-#sh /Applications/CSS_Phoebus.app/phoebus-4.7.3-SNAPSHOT/phoebus.sh -resource "file://$PWD/EVG_sysmon.bob?app=display_runtime"
-sh /Applications/CSS_Phoebus.app/product-5.0.2/phoebus.sh -resource "file://$PWD/EVG_sysmon.bob?app=display_runtime"
+sh /Applications/CSS_Phoebus.app/product-5.0.2/phoebus.sh -resource "file://$PWD/ospreyEVT.bob?P=${P}"
